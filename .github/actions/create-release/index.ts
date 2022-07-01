ee2ead7dde9ef573c07d4d0b351f0b4d1ca6db5f
lukehedger <lh4@pm.me>
2022-07-01 17:23:59 +0100

    Is it done?

--- .github/actions/create-release/index.ts
+++ .github/actions/create-release/index.ts
@@ -8,15 +8,65 @@ const octokit = new Octokit({
 
 const createRelease = async () => {
   try {
-    const body = "testing";
+    const { data: releases } = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: "lukehedger",
+        repo: "actions-auto-release-monorepo",
+      }
+    );
+
+    const [previousRelease] = releases
+      .filter((release) => {
+        return release.tag_name.includes(process.env.SERVICE_NAME);
+      })
+      .sort((releaseA, releaseB) => {
+        return (
+          new Date(releaseB.published_at).getTime() -
+          new Date(releaseA.published_at).getTime()
+        );
+      });
+
+    const [, previousReleaseSha] = previousRelease.tag_name.match(/-(.*)/);
+
+    const { data: commits } = await octokit.request(
+      "GET /repos/{owner}/{repo}/commits",
+      {
+        owner: "lukehedger",
+        path: process.env.SERVICE_PATH,
+        per_page: 100,
+        repo: "actions-auto-release-monorepo",
+        sha: "refs/heads/main",
+      }
+    );
+
+    const orderedCommits = commits.sort((commitA, commitB) => {
+      return (
+        new Date(commitB.commit.committer.date).getTime() -
+        new Date(commitA.commit.committer.date).getTime()
+      );
+    });
+
+    let releaseCommits = [];
+
+    for (const commit of orderedCommits) {
+      if (commit.sha.substring(0, 7) === previousReleaseSha) {
+        break;
+      }
+
+      releaseCommits.push(
+        `* ${commit.sha.substring(0, 7)} ${commit.commit.message}`
+      );
+    }
+
+    const releaseBody = releaseCommits.join("\n");
 
     const shortSha = context.sha.substring(0, 7);
 
-    // TODO: Would be good to get service name from package.json[name]
     const tagName = `${process.env.SERVICE_NAME}-${shortSha}`;
 
     await octokit.request("POST /repos/{owner}/{repo}/releases", {
-      body: body,
+      body: releaseBody,
       draft: process.env.INPUT_DRAFT === "true",
       generate_release_notes: false,
       name: process.env.NAME,
